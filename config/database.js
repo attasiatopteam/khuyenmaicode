f64794f6c1b5ca4f8b945c4992569252c3ddc88d
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2023-01-05 00:01:51 +0800

    .

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
 const connectDb = async()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/khuyenmaicode')
+        await mongoose.connect('mongodb://127.0.0.1/khuyenmaicode')
         // await mongoose.connect('mongodb+srv://irisattapp:Rythermbk98@cluster0.1p1h1vm.mongodb.net/kmcode_200?retryWrites=true&w=majority')
         console.log("Connect database succesfully")
     } catch (error) {
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const connectDb = require('./config/database')
+const bodyParser = require('body-parser')
 const Router = require('./routes')
 const app = express()
 const cors = require('cors')
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb',extended:true}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors())
 connectDb()
 Router(app)
