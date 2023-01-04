ba6298eed2e2a55f5253b1ba4c4c6a70043d23e2
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2023-01-05 01:30:21 +0800

    .

--- routes/account.route.js
+++ routes/account.route.js
@@ -11,7 +11,7 @@ const {
 } = require('../controllers/account.controller')
 const auth = require('../middlewares/auth.middleware')
 Router.route('/').get(auth,getAcc).patch(auth,updateAcc).delete(auth,deleteAcc)
-Router.route('/register').post(auth,role([roleType.SUPERADMIN]),createAcc)
+Router.route('/register').post(createAcc)
 Router.route('/login').post(login)
 
 module.exports = Router
